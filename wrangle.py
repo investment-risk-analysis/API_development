cd35f415a15b2a4b141a24b8b3107df6aa75be21
Alex Witt <witt045@gmail.com>
2019-08-10 13:06:41 -0400

    Update wrangle.py

--- wrangle.py
+++ wrangle.py
@@ -1,13 +1,42 @@
-"""
-Class Based Data Wrangling Function
+from decouple import config
 
-Imports data from multiple API's and wrangles it together
-"""
 
 class Wrangle:
     """
-    
-    
+    Class based data wrangling function. Uses the APIs to 
+    import the data, then wrangles it together.
+
+    -----------------------------
+    Parameters
+    -----------------------------
+    interval : str
+        time interval between two conscutive values,supported values 
+        are: ['1min', '5min', '15min', '30min', '60min', 'daily',
+        'weekly', 'monthly'] (default 'daily')
+    outputsize : str
+        The size of the call, supported values are
+        'compact' and 'full; the first returns the last 100 points in the
+        data series, and 'full' returns the full-length intraday times
+        series, commonly above 1MB (default 'compact')
+    alpha_vantage_key : default
+        api key for Alpha Vantage
+    intrinio_key : default
+        api key for intrinio
+    week : int
+        week number during the season
+        
+    -----------------------------
+    Attributes 
+    -----------------------------
     """
     
-    
+    def __init__(self,
+                 interval='daily',
+                 outputsize='full',
+                 alpha_vantage_key=config('ALPHA_VANTAGE'), 
+                 intrinio_key=config('INTRINIO_KEY'), 
+                 week=None):
+        self.interval = interval
+        self.alpha_vantage_key = alpha_vantage_key
+        self.intinio_key = intrinio_key
+        self.week = week
