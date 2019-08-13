57e6a06374f8faabee6903a7ccfeb174c5d6a20d
Alex Witt <witt045@gmail.com>
2019-08-13 17:33:40 -0400

    slight changes
    quandl is broken, forex isn't great.

--- wrangle.py
+++ wrangle.py
@@ -8,6 +8,7 @@ import quandl
 #pylint: disable=no-member
 #pylint: disable=unbalanced-tuple-unpacking
 
+#TODO Warnings about data loss
 #TODO ASSERT Testing
 #TODO Print Statements
 #TODO Clean Documentation
@@ -59,43 +60,27 @@ class Wrangle:
         self.intrinio_key = intrinio_key
 
 
-    def initiate(self, supp_symbol=None, primary_df=None, step='init',):
-    
-        # TODO ASSERT Error if step /= 'new' or 'add'
-        # TODO ASSERT Error if step == 'add' and no supplimental ticker is provided
-        # TODO ASSERT Error if step == 'add' and no primary dataframe is provided
+    def initiate(self):
 
         ts = TimeSeries(key=self.alpha_vantage_key, 
                         output_format='pandas')
 
-        if step == 'init':
-            symbol = self.symbol
-        else:
-            symbol = supp_symbol
-
-        data, meta_data = ts.get_daily(symbol=symbol, 
+        data, meta_data = ts.get_daily(symbol=self.symbol, 
                                        outputsize=self.outputsize)
 
         print(meta_data)
 
         data = data.rename(columns={
-                '1. open'  : symbol+' open', 
-                '2. high'  : symbol+' high', 
-                '3. low'   : symbol+' low', 
-                '4. close' : symbol+' close', 
-                '5. volume': symbol+' volume'
+                '1. open'  : self.symbol+' open', 
+                '2. high'  : self.symbol+' high', 
+                '3. low'   : self.symbol+' low', 
+                '4. close' : self.symbol+' close', 
+                '5. volume': self.symbol+' volume'
         }
     )
-        if step == 'init':
-            return data
+        return data
 
-        else:
-            final_df = primary_df.merge(data, 
-                                        how='inner', 
-                                        on='date')
-            return final_df
-
-    def bulk_add_securities(self, symbols, primary_df):
+    def add_securities(self, symbols, primary_df):
         # TODO ASSERT symbols is a list:
 
         ts = TimeSeries(key=self.alpha_vantage_key, 
@@ -174,7 +159,8 @@ class Wrangle:
                              output_format='pandas')
         
         data, meta_data = cc.get_currency_exchange_daily(from_symbol=from_currency, 
-                                                         to_symbol=to_currency)
+                                                         to_symbol=to_currency,
+                                                         outputsize=self.outputsize)
 
         print(meta_data)
 
